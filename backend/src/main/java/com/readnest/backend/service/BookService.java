9d562342343c95ca7fa1dcd0d1e203d1f0b4e347
Ahmad Jsy <ahmadaljaghssy@gmail.com>
2025-03-28 16:53:50 -0500

    Implemented Book Catalog API (Entity, Repository, Service, Controller)

---
+++ backend/src/main/java/com/readnest/backend/service/BookService.java
@@ -0,0 +1,39 @@
+package com.readnest.backend.service;
+
+import com.readnest.backend.model.Book;
+import com.readnest.backend.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Long id, Book bookDetails) {
+        Book book = bookRepository.findById(id).orElseThrow();
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+        book.setCategory(bookDetails.getCategory());
+        book.setTotalPages(bookDetails.getTotalPages());
+        book.setPagesRead(bookDetails.getPagesRead());
+        book.setNotes(bookDetails.getNotes());
+        book.setRating(bookDetails.getRating());
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
